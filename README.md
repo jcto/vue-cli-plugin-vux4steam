16aeddfdba6652af91fabf9efe3ca0d7edb50f53
System Administrator <root@OPGdeMacBook-Pro.local>
2018-08-22 14:00:45 +0800

    初始化代码

---
+++ README.md
@@ -0,0 +1,11 @@
+# vue-cli-plugin-vux
+
+vue-cli 3.0 vux 插件，快速安装 vux 到你的项目中
+
+## 开始
+
+```
+# 安装并调用 vue-cli-plugin-vux
+$ vue add vux
+```
+[参考插件](https://github.com/hookszhang/vue-cli-plugin-vux)
---
+++ generator.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+
+module.exports = (api, options, rootOptions) => {
+  const appVueContent = readAppVue()
+  rootOptions.appVueContent = appVueContent
+
+  api.render('./template')
+
+  // 修改 `package.json` 里的字段
+  api.extendPackage({
+    dependencies: {
+      'vux': '^2.9.2'
+    },
+    devDependencies: {
+      'less': '^3.8.1',
+      'less-loader': '^4.1.0',
+      'vue-loader': '14.2.3',
+      'vue-template-compiler': '^2.5.17',
+      'vux-loader': '^1.2.9'
+    }
+  })
+}
+
+function readAppVue () {
+  const content = fs.readFileSync('src/views/om/App.vue', { encoding: 'utf-8' })
+  return content
+}
